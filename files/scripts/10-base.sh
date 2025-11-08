19ed120441bf4bc8fc8714ce4b53050ba43d5d07
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-08 16:30:22 -0300

    Update 10-base.sh

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -19,6 +19,10 @@ dnf install ublue-brew ublue-os-udev-rules -y
 dnf install ublue-polkit-rules ublue-os-just ublue-os-update-services ublue-setup-services uupd ublue-bling ublue-rebase-helper -y
 dnf copr disable ublue-os/packages -y
 
+dnf copr enable sabbyblue/branding -y
+dnf install fedora-logos -y
+dnf copr disable sabbyblue/branding -y
+
 systemctl enable brew-setup.service
 systemctl disable mcelog.service
 systemctl --global enable podman-auto-update.timer
